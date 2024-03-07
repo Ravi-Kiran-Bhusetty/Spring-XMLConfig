d889081e8552bd1702b29b6f9cde1c3f1b5a22f0
Ravi-Kiran-Bhusetty <ravikiranb.2205@gmail.com>
2024-03-07 18:34:01 +0530

    Lazy initialization: Desktop Class: bean lazy-init

---
+++ src/main/java/org/example/Desktop.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Desktop {
+
+    public Desktop() {
+
+        System.out.println("Desktop Object Created");
+    }
+
+    public void compile() {
+
+        System.out.println("Compiling from Desktop");
+    }
+}
